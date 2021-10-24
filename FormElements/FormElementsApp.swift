fca9ac1c0ae63323ef710ce230a01e847f539f94
farkhod <986532qw@gmail.com>
2021-10-24 22:07:36 +0300

    Initial Commit

---
+++ FormElements/FormElementsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FormElementsApp.swift
+//  FormElements
+//
+//  Created by Farkhod on 24.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct FormElementsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
