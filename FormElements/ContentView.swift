758dfc6e7fcc9e879b2aaeb800a298b85dccb3b6
farkhod <986532qw@gmail.com>
2021-10-25 20:09:29 +0300

    Form with user data entering elements. Including keybord desmiss

--- FormElements/ContentView.swift
+++ FormElements/ContentView.swift
@@ -8,9 +8,52 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var firstName: String = ""
+    @State private var lastName: String = ""
+    @State private var birthday = Date()
+    @State private var shouldSendNews = false
+    @State private var numberOfLikes = 1
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            Form {
+                Section(header: Text("Personal information")) {
+                    TextField("First name", text: $firstName)
+                    TextField("Last name", text: $lastName)
+                    DatePicker("Birthday", selection: $birthday, displayedComponents: .date)
+                }
+                
+                Section(header: Text("Actions")) {
+                    Toggle("Send News", isOn: $shouldSendNews)
+                        .toggleStyle(SwitchToggleStyle(tint: .red))
+                    Stepper("Number of Likes", value: $numberOfLikes, in: 1...100)
+                    Text("This video has \(numberOfLikes) likes")
+                    Link("Privacy policy", destination: URL(string: "https://github.com/farkhodkh")!)
+                }
+            }
+            
+            .navigationTitle("Account")
+            .toolbar {
+                ToolbarItemGroup (placement: .navigationBarTrailing) {
+                    Button() {
+                        hideKeyboard()
+                    } label: {
+                        Image(systemName: "keyboard.chevron.compact.down")
+                    }
+                    
+                    Button() {
+                        saveUser()
+                    } label: {
+                        Text("Save")
+                    }
+                }
+            }
+        }
+        .accentColor(.red)
+    }
+    
+    func saveUser() {
+        print("User saved")
     }
 }
 
@@ -19,3 +62,12 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+#if canImport(UIKit)
+extension View {
+    func hideKeyboard() {
+        UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+    }
+}
+#endif
+
